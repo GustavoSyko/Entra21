5353bb01174722000d2fc51962293ebeb70aa0c0
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-08-23 16:03:30 -0300

    .

---
+++ 23-08-23/script.js
@@ -0,0 +1,19 @@
+/*
+
+2023-08-23
+
+Exercícios de Laço de Repetição
+
+
+Desenvolva uma aplicação que permita informar 
+as notas das avaliações de dois alunos.
+Cada aluno tem duas avaliações.
+Calcule a média e informe se está aprovado ou reprovado.
+Aprovado  -> média >= 7
+Reprovado -> média < 7
+
+*/
+
+for(var notas = 1; notas >= 3; notas++){
+    prompt("Qual a nota do aluno 1 ? ")
+}
---
+++ 23-08-23/scriptdoprof.js
@@ -0,0 +1,9 @@
+// Variavel para controlar qtde de alunos
+
+var qtdAlunos 
+var nomeAluno
+
+// Laço para controlar leitura de alunos
+while (qtdAlunos <= 2 ){
+    nomeAluno = prompt('Informe o nome do aluno')
+}
