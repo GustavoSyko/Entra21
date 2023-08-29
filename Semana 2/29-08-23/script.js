acdbfd3bed63336dbbe27ab1818d4a71774f7faf
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-08-29 15:59:15 -0300

    .

---
+++ Semana 2/29-08-23/script.js
@@ -0,0 +1,24 @@
+//          ARRAYS  ----  VETORES     
+
+
+var vetNomes = ["Paola", "Amanda", "João", "Eduardo"]
+var vetIdades = [16, 15, 17, 16]
+var vetPassou = [true, false, true, false]
+
+// Lembre-se que os vetores/matrizes(arrays)
+// sempre começam na posição 0
+
+// Localizando a amanda
+console.log(vetNomes[1])
+
+//localizando a idade da Amanda
+console.log(vetIdades[1])
+
+// Localizando se a Amanda passou de ano
+console.log(vetPassou[1])
+
+// Localizando o ultimo nome com base no tamanho do Array
+console.log(vetNomes[vetNomes.length-1])
+
+// Usando o -1 porque o array começa no 0 , então será o tamanho total,menos 1 
+// que seria o número 3 .
